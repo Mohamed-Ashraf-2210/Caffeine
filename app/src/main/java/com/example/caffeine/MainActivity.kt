4fcf9700bca550de88ccc4c6a0c415643483db41
Mohamed Ashraf <ma1261327@gmail.com>
2025-07-03 18:10:23 +0300

    Implement navigation and add Welcome screens; create NavigationStack and WelcomeOneScreen components

--- app/src/main/java/com/example/caffeine/MainActivity.kt
+++ app/src/main/java/com/example/caffeine/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.caffeine.navigation.Screen
+import com.example.caffeine.presentation.WelcomeOneScreen
 import com.example.caffeine.ui.theme.CaffeineTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,13 +18,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CaffeineTheme {
-                Text(
-                    text = "Hello Caffeine!",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp)
-                )
+                NavigationStack()
             }
         }
+    }
+}
+
+@Composable
+fun NavigationStack() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.WelcomeOne.route) {
+        composable(Screen.WelcomeOne.route) { WelcomeOneScreen(navController) }
+        composable(Screen.WelcomeTwo.route) { WelcomeOneScreen(navController) }
     }
 }
